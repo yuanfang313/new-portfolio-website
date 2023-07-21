85c68a29343d0e105948ad856b3c03c37e527341
Yuan Fang <yxf313@miami.edu>
2023-07-21 17:31:13 +0800

    clean css

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ let header = document.getElementById("myHeader");
 let sticky = header.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if (window.scrollY > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
+
+const links = document.querySelectorAll(".projectLink");
+const githubURL = "https://yuanfang313.github.io/new-portfolio-website";
+const localURL = "";
+let realURL = localURL;
+
+links.forEach((link) => {
+  let href = link.getAttribute("href");
+  let newHref = realURL + href;
+  link.setAttribute("href", newHref);
+});
