d5bc2cca4e9f4890a8e8648fe762a71dc6a11b5c
Yuan Fang <42786261+yuanfang313@users.noreply.github.com>
2023-07-29 20:56:19 +0800

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 "use strict";
 
+// create header
+const headerHtml =
+  '<div class="header-bar item left"><a href="/" class="projectLink">Fang Yuan</a></div><div class="header-bar item right"><ul class="menu align-right"><li><a href="/" class="projectLink">Works</a></li><li><a href="/">About</a></li></ul></div>';
+
+const copyrightHtml =
+  '<div class="copyright item"><a href="/" class="projectLink">© Fang Yuan</a></div>';
+
+document
+  .querySelector("#myHeader")
+  .insertAdjacentHTML("afterbegin", headerHtml);
+
+document
+  .querySelector("#myCopyright")
+  .insertAdjacentHTML("afterbegin", copyrightHtml);
+
+// create sticky header
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
+
 window.onscroll = function () {
   myFunction();
 };
-
-let header = document.getElementById("myHeader");
-let sticky = header.offsetTop;
 
 function myFunction() {
   if (window.scrollY > sticky) {
@@ -15,6 +31,7 @@ function myFunction() {
   }
 }
 
+// toggle site url
 const links = document.querySelectorAll(".projectLink");
 const githubURL = "https://yuanfang313.github.io/new-portfolio-website";
 const localURL = "";
